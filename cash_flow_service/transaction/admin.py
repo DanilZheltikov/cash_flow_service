158dc79ec2afdca987a1ca7b972410d94efbdf94
Danil Zheltikov <hikikomoridaz@gmail.com>
2026-06-28 17:54:09 +0700

    feat(admin): add admin interface with model registrations and customizations

--- cash_flow_service/transaction/admin.py
+++ cash_flow_service/transaction/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Category,
+    Subcategory,
+    Transaction,
+    TransactionStatus,
+    TransactionType
+)
+
+
+class BaseAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = [
+        'created_at',
+        'status',
+        'transaction_type',
+        'category',
+        'subcategory',
+        'amount'
+    ]
+    search_fields = [
+        'category__name',
+        'status__name',
+        'subcategory__name',
+        'transaction_type__name'
+    ]
+    list_filter = ['created_at']
+
+
+@admin.register(TransactionStatus)
+class TransactionStatusAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(TransactionType)
+class TransactionTypeAdmin(BaseAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(BaseAdmin):
+    list_display = [*BaseAdmin.list_display, 'transaction_type']
+
+
+@admin.register(Subcategory)
+class SubcategoryAdmin(BaseAdmin):
+    list_display = [*BaseAdmin.list_display, 'category']
