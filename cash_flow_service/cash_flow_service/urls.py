7c8099a423fba45d96284aebcb17ce73768d7148
Danil Zheltikov <hikikomoridaz@gmail.com>
2026-06-28 10:02:19 +0700

    fix(urls): correct syntax for including transaction URLs with namespace

--- cash_flow_service/cash_flow_service/urls.py
+++ cash_flow_service/cash_flow_service/urls.py
@@ -3,5 +3,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('transaction.urls'), namespace='transaction'),
+    path('', include('transaction.urls', namespace='transaction')),
 ]
