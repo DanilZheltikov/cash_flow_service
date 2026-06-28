d7b817de7cec1a3e34884dd51d98dbf73f62fb30
Danil Zheltikov <hikikomoridaz@gmail.com>
2026-06-28 21:15:28 +0700

    feat(settings, urls): add smart_selects to INSTALLED_APPS and configure URL routing

--- cash_flow_service/cash_flow_service/settings.py
+++ cash_flow_service/cash_flow_service/settings.py
@@ -15,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'smart_selects',
     'django_bootstrap5',
     'django_filters',
     'transaction.apps.TransactionConfig',
--- cash_flow_service/cash_flow_service/urls.py
+++ cash_flow_service/cash_flow_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chaining/', include('smart_selects.urls')),
     path('', include('transaction.urls', namespace='transaction')),
 ]
